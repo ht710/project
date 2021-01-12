c8d22dc403dfcdd454a8ecf2f369473c0920b5cd
hanting <lt1374432048@163.com>
2021-01-12 17:43:48 +0800

    文章管理模块完成

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -50,9 +50,73 @@ $(function () {
             }
         })
     })
+    // 通过事件委托的形式  为btn-edit 按钮绑定点击事件
+    var indexEdit = null
+    $("tbody").on("click", ".btn-edit", function () {
+        indexEdit = layui.layer.open({
+            type: 1,
+            area: ["500px", "250px"],
+            title: "修改文章分类",
+            content: $("#dialog-edit").html()
+        })
+        // 为修改文章分类的 弹出层填充表单数据  当弹出层展示出来之后  根据id的值发起请求获取文章分类的数据
+        // 获取自定义属性值   attr("属性名称")
+        var id = $(this).attr("data-id")
+        // 发起ajax请求
+        $.ajax({
+            method: "GET",
+            url: "/my/article/cates/" + id,
+            success: function (res) {
+                // console.log(res);   
+                // data: { Id: 1, name: "科技", alias: "科dddsds", is_delete: 0 }
+                layui.form.val("form-edit", res.data)
+            }
+        })
+    })
+    // 通过事件委托  给修改按钮绑定submit提交事件  发起ajax请求  status等于0，则表示编辑成功
+    $("body").on("submit", "#form-edit", function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: "POST",
+            url: "/my/article/updatecate",
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layui.layer.msg("更新分类信息失败！")
+                }
+                layui.layer.msg("更新分类信息成功！")
+                // 关闭弹出层
+                layui.layer.close(indexEdit)
+                // 调用initArtCateList()获取最新的数据
+                initArtCateList()
+            }
+        })
+    })
+    // 通过代理的形式 为删除按钮绑定点击事件
+    $("tbody").on("click", ".btn-delete", function () {
+        // 获取自定义属性值   attr("属性名称")  通过拿到id来删除文章分类
+        var id = $(this).attr("data-id")
+        // 提示用户是否要删除
+        // confirm 询问框复制
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            $.ajax({
+                method: "GET",
+                url: "/my/article/deletecate/" + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layui.layer.msg("删除文章分类失败！")
+                    }
+                    layui.layer.msg("删除文章分类成功！")
+                    layer.close(index);
+                    // 调用initArtCateList()获取最新的数据
+                    initArtCateList()
+                }
+            })
+
+        })
 
 
-
-
-
+    })
 })
